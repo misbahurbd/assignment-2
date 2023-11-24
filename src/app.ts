396aa9ab77ece65a3b5d0db89139f245f96aa1ee
Misbahur Rahman <misbahurbd@gmail.com>
2023-11-25 04:37:25 +0600

    create not found route error handler

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import { userRouter } from './modules/user/user.router';
 
@@ -9,7 +9,24 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
+// initial response setup
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    message: 'Welcome to CRUD API',
+    data: null,
+  });
+});
+
 // initialize routes
 app.use('/api/users', userRouter);
+
+// handle not found route error
+app.all('*', (req: Request, res: Response) => {
+  res.status(404).json({
+    success: false,
+    message: 'Route not found',
+  });
+});
 
 export default app;
